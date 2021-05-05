a4eda081715ab710a2fc75f31e1da0375633502f
marcelimati <38301040+marcelimati@users.noreply.github.com>
2021-05-05 17:56:29 +0200

    Update jwt.strategy.ts

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: '7dTSPimW1C34o6vEZIors2sga',
+      secretOrKey: process.env.secret,
     });
   }
 
